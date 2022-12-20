a270d9791c22cb9a758cc156d73c7bd1efc0c72b
nic96 <nic96@users.noreply.github.com>
2022-12-20 14:07:36 -0700

    fixed death message color

--- Main.cs
+++ Main.cs
@@ -31,7 +31,7 @@ namespace coolpuppy24.simpledeathmessages
                 {"food","{0} starved to death!"},
                 {"arena","{0} was eliminated by the arena!"},
                 {"shred","{0} was shreaded to bits!"},
-                {"punch","{0} was punched to death in the {1} by {2}"},
+                {"punch","{0} was punched in the {1} by {2}"},
                 {"bones","{0} shattered his bones and died!"},
                 {"melee","{0} was chopped to death in the {1} by {2} using a {3}"},
                 {"water","{0} dehydrated to death!"},
@@ -40,14 +40,14 @@ namespace coolpuppy24.simpledeathmessages
                 {"animal","{0} was mauled by an animal!"},
                 {"grenade","{0} was blown up by {1} with a grenade!"},
                 {"vehicle","{0} was blown up by a vehicle!"},
-                {"suicide","{0} killed himself!"},
+                {"suicide","{0} died at their own hand. Everyone is disappointed."},
                 {"burning","{0} burned to death!"},
                 {"landmine","{0} was blown up by a landmine!"},
                 {"roadkill","{0} was roadkilled by {1}"},
                 {"bleeding","{0} bled to death!"},
                 {"freezing","{0} froze to death!"},
                 {"sentry","{0} was shot by a sentry gun!"},
-                {"charge","{0} was blown up by {1} with a remote detonator!"},
+                {"charge","{0} was obliterated by {1} with a remote detonator!"},
                 {"missile","{0} was annihilated by {1} with a missile!"},
                 {"splash","{0} was blown up by {1} with an explosive bullet!"},
                 {"acid","{0} was blown up by a zombie!"},
@@ -56,9 +56,19 @@ namespace coolpuppy24.simpledeathmessages
                 {"spit","{0} was dissolved by a zombie!"},
                 {"kill","{0} was killed by an admin!"},
                 {"boulder","{0} was crushed by a zombie using a big boulder!"},
-                {"leg", "leg"},
-                {"arm", "arm"},
-                {"spine", "torso"},
+                {"left_foot", "left foot"},
+                {"left_leg", "left leg"},
+                {"right_foot", "right foot"},
+                {"right_leg", "right leg"},
+                {"left_hand", "left hand"},
+                {"left_arm", "left arm"},
+                {"right_hand", "right hand"},
+                {"right_arm", "right arm"},
+                {"left_back", "back"},
+                {"right_back", "back"},
+                {"left_front", "torso"},
+                {"right_front", "torso"},
+                {"spine", "spine"},
                 {"skull", "head"},
             };
 
@@ -93,13 +103,15 @@ namespace coolpuppy24.simpledeathmessages
                     case "MISSILE":
                     case "CHARGE":
                     case "SPLASH":
-                    case "PUNCH":
                         UnturnedChat.Say(Translate(cause.ToString().ToLower(), player.DisplayName, killer.DisplayName), deathMessageColor);
+                        break;
+                    case "PUNCH":
+                        UnturnedChat.Say(Translate(cause.ToString().ToLower(), player.DisplayName, Translate(limb.ToString().ToLower()), killer.DisplayName), deathMessageColor);
                         break;
                     case "MELEE":
                     case "GUN":
                         var limbString = Translate(limb.ToString().ToLower());
-                        UnturnedChat.Say(Translate(cause.ToString().ToLower(), player.DisplayName, Translate(limbString), killer.DisplayName, killer.Player.equipment.asset.itemName));
+                        UnturnedChat.Say(Translate(cause.ToString().ToLower(), player.DisplayName, limbString, killer.DisplayName, killer.Player.equipment.asset.itemName), deathMessageColor);
                         break;
                 }
 
